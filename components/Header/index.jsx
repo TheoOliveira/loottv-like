98ac8d19659a8a9138a13b3e96b65f4af4ef15f6
TheoOliveira <theophilo.oliveira@gmail.com>
2022-06-24 19:21:57 -0300

    mobile version

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import Searchbar from '../Searchbar/index'
 import styles from './Header.module.scss'
@@ -8,23 +8,31 @@ import Logo from '../Logo'
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false)
 
-  function openSidebar(){
+  function openSidebar() {
     setIsOpen(true)
-      const sidebar = document.querySelector('.Sidebar_container__mO_GZ').style.width = "0%"
-      const grid = document.querySelector('.VideoGrid_container__5Eqyd').style.width= "100%"
+    const sidebar = document.querySelector('.Sidebar_container__mO_GZ').style.width = "0%"
+    const grid = document.querySelector('.VideoGrid_container__5Eqyd').style.width = "100%"
   }
-  
-  function closeSidebar(){
+
+  function closeSidebar() {
     setIsOpen(false)
-    const sidebar = document.querySelector('.Sidebar_container__mO_GZ').style.width = "10%"
-    const grid = document.querySelector('.VideoGrid_container__5Eqyd').style.width= "90%"
+    let w = window.innerWidth;
+    console.log(w)
+    if (w >= 800) {
+      console.log(w)
+      const sidebar = document.querySelector('.Sidebar_container__mO_GZ').style.width = "10%"
+      const grid = document.querySelector('.VideoGrid_container__5Eqyd').style.width = "90%"
+    }else {
+      const sidebar = document.querySelector('.Sidebar_container__mO_GZ').style.width = "30%"
+      const grid = document.querySelector('.VideoGrid_container__5Eqyd').style.width = "70%"
+    }
   }
   return (
     <header className={styles.container}>
       <div className={styles.containerLogoHam}>
-      <button className={styles.hamButton} onClick={() => !isOpen ? openSidebar() : closeSidebar()}>
-      </button>
-      <Logo/>
+        <button className={styles.hamButton} onClick={() => !isOpen ? openSidebar() : closeSidebar()}>
+        </button>
+        <Logo />
       </div>
       <Searchbar />
     </header>
