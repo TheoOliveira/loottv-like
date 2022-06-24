818502633dc5afd9f0d2270b8da732e1261d5832
TheoOliveira <theophilo.oliveira@gmail.com>
2022-06-24 09:02:11 -0300

    new image and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 // pages/_app.js
 import '../styles/reset.scss'
 import '../styles/base.scss'
+import Head from "next/head"
+
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+         <Head>
+          <link rel="icon" href="/favicon.png" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
       <Component {...pageProps} />
+      </>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
 import VideoGrid from '../components/VideoGrid'
+
 function HomePage() {
     return <div>
       <Header />
