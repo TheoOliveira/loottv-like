75d94681cdfc55f5e122a4db1036762779e81a15
TheoOliveira <theophilo.oliveira@gmail.com>
2022-07-18 10:49:43 -0300

    working env

--- api/firebase.js
+++ api/firebase.js
@@ -1,8 +1,15 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, initializeFirestore } from "firebase/firestore";
-import {firebaseConfig} from "./keys";
+import { initializeFirestore } from "firebase/firestore";
+// import {firebaseConfig} from "./keys";
 
-
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE,
+  messagingSenderId: process.env.NEXT_PUBLIC_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+};
 
 const  app = initializeApp(firebaseConfig)
 const db = initializeFirestore(app, {
